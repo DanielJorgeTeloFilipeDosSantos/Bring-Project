993750e87765744dae30aa40b78681c3ad0b9709
Daniel Santos <danieljorgefilipesantos@gmail.com>
2019-10-02 23:42:46 +0100

    added react components for navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavigationBar from "./components/NavigationBar";
 
 import "./App.css";
 
@@ -7,14 +9,19 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomeView from "./views/Home";
 import ErrorView from "./views/Error";
 import CatchAllView from "./views/CatchAll";
+import Register from "./views/Register";
+import Signin from "./views/SigninDonor";
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <Router>
+          <NavigationBar />
           <Switch>
             <Route path="/" exact component={HomeView} />
+            <Route path="/register" exact component={Register} />
+            <Route path="/signin" exact component={Signin} />
             <Route path="/error/:code" component={ErrorView} />
             <Route path="/" component={CatchAllView} />
           </Switch>
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
+import people from "../assets/People.svg";
+import Container from "react-bootstrap/Container";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
 export default class HomeView extends Component {
   render() {
     return (
       <div>
-        <h1>default</h1>
+        <Container>
+          <img
+            src={people}
+            style={{ margin: "5vh", marginTop: "15vh" }}
+            alt="Logo"
+          />
+        </Container>
+
+        <Container>
+          <h2>Help us give to who has less</h2>
+        </Container>
+        <Link to="/register">
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ margin: "2vh", marginTop: "4vh" }}
+          >
+            I have something to Donate
+          </Button>
+        </Link>
+        <Container>
+          <Button variant="contained" color="primary" style={{ margin: "2vh" }}>
+            I want to do Volunteering
+          </Button>
+        </Container>
       </div>
     );
   }
