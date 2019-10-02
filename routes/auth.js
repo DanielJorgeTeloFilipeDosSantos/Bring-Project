a341662822c3b1db3e03655a80d375d3286c7199
Daniel Santos <danieljorgefilipesantos@gmail.com>
2019-10-02 11:00:55 +0100

    a

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const { Router } = require("express");
+const router = Router();
+
+//const uploadImageMiddleware = require("./../middleware/image-upload");
+const routeGuardMiddleware = require("./../middleware/route-guard");
+
+module.exports = router;
