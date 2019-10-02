b9c16c22c3a0d706590f3625f8d3b25fa11e5b83
Daniel Santos <danieljorgefilipesantos@gmail.com>
2019-10-02 16:42:55 +0100

    a

--- models/donorUser.js
+++ models/donorUser.js
@@ -2,30 +2,32 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const DonorSchema = new Schema(
-  {
-    name: {
-      type: String,
+const DonorSchema = new Schema({
+  name: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    unique: true,
 
-      required: true
-    },
-
-    password: {
-      type: String,
-
-      required: true
-    },
-    foto: {
-      type: String,
-      required: true
-    },
-    date: {
-      type: Date,
-
-      default: Date.now
-    }
+    required: true
   },
-  { versionKey: false }
-);
+
+  password: {
+    type: String,
+
+    required: true
+  },
+  foto: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true,
+    unique: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 module.exports = donorUser = mongoose.model("donors", DonorSchema);
